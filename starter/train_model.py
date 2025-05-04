4712be8f13dc253cb96df4c5d2f881ad39f4d2c0
Daniele Sciretti <sciretti@gmail.com>
2025-05-04 21:44:33 +0200

    Add metric on slices

--- starter/train_model.py
+++ starter/train_model.py
@@ -32,15 +32,22 @@ logger = logging.getLogger()
 def load_and_preprocess_data(dataset_name='./data/census_clean.csv'):
     logger.info(f"Loading data from {dataset_name}")
     data = pd.read_csv(dataset_name)
-    # TODO Remove duplicate lines
     data = data.drop_duplicates()
-    # TODO Check values out of range?
-    # Drop outliers
-    # min_price = 10
-    # max_price = 350
-    # idx = df['price'].between(min_price, max_price)
-    # df = df[idx].copy()
     return data
+
+def model_slices(model, data, categorical_features, encoder, lb):
+
+    for cat in categorical_features:
+        for val in data[cat].unique():
+            data_tmp = data[data[cat] == val]
+            X, y, _, _ = process_data(
+                data_tmp, categorical_features=categorical_features,
+                label="salary", encoder=encoder, lb=lb, training=False)
+            y_pred = inference(model, X)
+            precision, recall, fbeta = compute_model_metrics(y, y_pred)
+            msg = f" == category {cat}/{val}: "
+            msg += f"prec={precision:.2f}, recall={recall:.2f}, fbeta={fbeta:.2f}"
+            logging.info(msg)
 
 
 if __name__ == "__main__":
@@ -72,14 +79,14 @@ if __name__ == "__main__":
     )
 
     # Save preprocessing, e.g. encoder
-    joblib.dump(encoder, './model/encoder.pkl')
-    joblib.dump(lb, './model/lb.pkl')
+    joblib.dump(encoder, './model/encoder_test.pkl')
+    joblib.dump(lb, './model/lb_test.pkl')
 
     # Train and save a model.
     logging.info("Training ML model")
     model = train_model(X_train, y_train)
 
-    model_name = './model/rfc_model.pkl'
+    model_name = './model/rfc_model_test.pkl'
     logging.info(f"Saving ML model to file {model_name}")
     joblib.dump(model, model_name)
 
@@ -88,11 +95,14 @@ if __name__ == "__main__":
     y_train_pred = inference(model, X_train)
     precision, recall, fbeta = compute_model_metrics(y_train, y_train_pred)
 
-    logging.info("  Metrics on training data:")
-    logging.info(f"    prec={precision}, recall={recall}, fbeta={fbeta}")
+    logging.info(" Metrics on training data:")
+    logging.info(f" prec={precision:.2f}, recall={recall:.2f}, fbeta={fbeta:.2f}")
 
     y_test_pred = inference(model, X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, y_test_pred)
 
-    logging.info("  Metrics on test data:")
-    logging.info(f"    prec={precision}, recall={recall}, fbeta={fbeta}")
+    logging.info(" Metrics on test data:")
+    logging.info(f" prec={precision:.2f}, recall={recall:.2f}, fbeta={fbeta:.2f}")
+
+    logging.info(" Metrics on slices:")
+    model_slices(model, data, cat_features, encoder, lb)
